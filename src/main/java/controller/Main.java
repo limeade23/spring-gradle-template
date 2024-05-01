3a34024faa71fd9a64e1ebd4324fb9f843899a1e
limeade23 <junhan.koo@limeade.me>
2024-05-02 00:54:05 +0900

    file add

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Main {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    }
+
+}
